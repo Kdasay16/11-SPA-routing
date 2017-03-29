318369443d4d9bff236f79f16b497c50afe91b8e
Logan Absher <absherlogan@gmail.com>
2017-03-28 17:29:19 -0700

    sretch goals

---
+++ starter-code/public/scripts/controllers/adminController.js
@@ -0,0 +1,9 @@
+// 'use strict';
+// console.log('yes');
+// (function(module) {
+//   const adminController = {};
+//   adminController.init = function() {
+//     console.log('yes');
+//   }
+//   module.adminController = adminController;
+// })(window);
